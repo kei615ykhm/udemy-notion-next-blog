2970ff4db9d144deee1290f14ede7451585cb69e
kei <from0615.ykhm@gmail.com>
2024-07-23 00:11:52 +0900

    wip: 作業中断

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-inport type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return <></>;
